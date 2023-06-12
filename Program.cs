33915509de23b06e69d5eba2c3aac84f1b1e8f46
MRJackFrost <69490107+MRJackFrost@users.noreply.github.com>
2023-06-12 12:49:25 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,51 +4,51 @@ using System.Globalization;
 namespace Course
 {
 
-    public class Triangulo
+    public class Triangulo //defindo a classe publica triangulo
     { 
-        public double A { get; set; }
-        public double B { get; set; }
-        public double C { get; set; } 
+        public double A { get; set; } //criando variaveis publicas para essa classe
+        public double B { get; set; } //criando variaveis publicas para essa classe
+        public double C { get; set; } //criando variaveis publicas para essa classe
 
-        public double CalcularArea()
+        public double CalcularArea() //definindo uma função publica da classe triangulo
         {
-            double p = (A + B + C)/2.0;
-            double area = Math.Sqrt(p *(p - A) * (p - B) * (p - C));
-            return area;
+            double p = (A + B + C)/2.0; //calculo
+            double area = Math.Sqrt(p *(p - A) * (p - B) * (p - C)); //calculo com raiz quadrada
+            return area; //retorne a area
         }
     }
 
-    public class Program
+    public class Program //abrindo a classe principal
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) //abrindo o bloco principal
         {
-            Triangulo x = LerTriangulo("X");
-            Triangulo y = LerTriangulo("Y");
+            Triangulo x = LerTriangulo("X"); //criando um objeto da classe triangulo
+            Triangulo y = LerTriangulo("Y"); //criando um objeto da classe triangulo
 
-            double areax = x.CalcularArea();
-            double areay = y.CalcularArea();
+            double areax = x.CalcularArea(); //definindo uma variavel e chamando uma função da outra classe
+            double areay = y.CalcularArea(); //definindo uma variavel e chamando uma função da outra classe
 
-            Console.WriteLine("Area de X: " + areax.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("Area de Y: " + areay.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area de X: " + areax.ToString("F4", CultureInfo.InvariantCulture)); //mostrando na interface do usuario
+            Console.WriteLine("Area de Y: " + areay.ToString("F4", CultureInfo.InvariantCulture)); //mostrando na interface do usuario
 
-            if (areax > areay)
+            if (areax > areay)// se a area de x for maior
             {
-                Console.WriteLine("Area de X é maior ");
+                Console.WriteLine("Area de X é maior "); //mostre para o usuario
             }
-            else
+            else //se não
             {
-                Console.WriteLine("Area de Y é maior ");
+                Console.WriteLine("Area de Y é maior "); //mostre para o usuario
             }
         }
-        public static Triangulo LerTriangulo(string nomeTriangulo)
+        public static Triangulo LerTriangulo(string nomeTriangulo) //Defindo outra função da classe triangulo
         { 
-            Triangulo tri = new Triangulo();
-            Console.WriteLine("Entre com os valores do triangulo " + nomeTriangulo);
-            tri.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            tri.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            tri.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Triangulo tri = new Triangulo(); //defindo um objeto para chamar as variaveis publicas
+            Console.WriteLine("Entre com os valores do triangulo " + nomeTriangulo); //mostre na tela do usario
+            tri.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //pedindo para o usuario inputar o valor
+            tri.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //pedindo para o usuario inputar o valor
+            tri.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //pedindo para o usuario inputar o valor
 
-            return tri;
+            return tri; //retorne o objeto
         }
     }
 }
